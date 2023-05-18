616bc362c8c9721154a0be56acee21e5b4f65dcd
MelonC21 <melvinsc@uci.edu>
2023-05-18 03:08:37 -0700

    worked on boxlevel

--- HappyMeowdays!/Assets/Scripts/SpawnObjectsOnActivation.cs
+++ HappyMeowdays!/Assets/Scripts/SpawnObjectsOnActivation.cs
@@ -1,3 +1,4 @@
+using System; 
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,26 +8,34 @@ public class SpawnObjectsOnActivation : MonoBehaviour
     public GameObject objectToSpawn; // The object to spawn when all objects are activated
     public Transform spawnPosition; // The position where the object should be spawned
 
-    private bool allObjectsActivated; // Flag indicating if all objects are activated
+    private int activatedObjectCount; // Number of activated objects
+
+    public event Action OnAllObjectsActivated; // Event triggered when all objects are activated
+
+    private void Start()
+    {
+        // Initialize the count of activated objects
+        activatedObjectCount = 0;
+    }
 
     private void Update()
     {
-        // Reset the flag before checking
-        allObjectsActivated = true;
-
-        // Check if all objects in the list are activated
+        // Subscribe to the event of each object
         foreach (GameObject obj in objectsToCheck)
         {
-            if (!obj.activeSelf)
-            {
-                allObjectsActivated = false;
-                break;
-            }
+            TriggerActivation triggerActivation = obj.AddComponent<TriggerActivation>();
+            triggerActivation.OnObjectActivated += HandleObjectActivated;
         }
+    }
 
-        // If all objects are activated, spawn the desired object
-        if (allObjectsActivated)
+    private void HandleObjectActivated()
+    {
+        activatedObjectCount++;
+
+        // Check if all objects are activated
+        if (activatedObjectCount >= objectsToCheck.Count)
         {
+            OnAllObjectsActivated?.Invoke();
             SpawnObject();
         }
     }
@@ -35,5 +44,19 @@ public class SpawnObjectsOnActivation : MonoBehaviour
     {
         Instantiate(objectToSpawn, spawnPosition.position, spawnPosition.rotation);
         // Add any additional logic or modifications to the spawned object as needed
+    }
+}
+
+public class TriggerActivation : MonoBehaviour
+{
+    public event Action OnObjectActivated; // Event triggered when the object is activated
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Change the tag to match your objects
+        {
+            gameObject.SetActive(true);
+            OnObjectActivated?.Invoke();
+        }
     }
 }
