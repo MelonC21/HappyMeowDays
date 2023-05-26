0460aa86da1a804f7add353da6c1da5b53f3c623
glibertt <sleestef@uci.edu>
2023-05-25 17:59:42 -0700

    this might explode

---
+++ HappyMeowdays!/Assets/Scripts/Text/DeleteName.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DeleteName : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public TextMeshPro speakerText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
