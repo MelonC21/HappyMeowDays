ada9f2fd07ba42329a636dd50e7d7e8b48946f6c
Keegannajera <keganrodriguez@gmail.com>
2023-06-07 21:36:23 -0700

    Revert "Merge remote-tracking branch 'origin/woahnewbranch' into Keegan2.0"
    This reverts commit c3c29940ce91bc2383777273da0dba51cfa4d99a, reversing
    changes made to 38030cdcf736238a08d12be405ec712f0e0dfe7d.

--- HappyMeowdays!/Assets/Scripts/PressurePlate.cs
+++ HappyMeowdays!/Assets/Scripts/PressurePlate.cs
@@ -11,8 +11,6 @@ public class PressurePlate : MonoBehaviour
 
     private bool isActivated = false;
 
-    public AudioClip buttonPush;
-
     private void OnTriggerEnter(Collider other)
     {
         if (!isActivated && other.CompareTag("Player"))
@@ -29,7 +27,5 @@ public class PressurePlate : MonoBehaviour
                 Destroy(gameObject);
             }
         }
-
-        AudioSource.PlayClipAtPoint(buttonPush, transform.position);
     }
 }
--- HappyMeowdays!/Assets/Scripts/PressurePlateTriggerScene.cs
+++ HappyMeowdays!/Assets/Scripts/PressurePlateTriggerScene.cs
@@ -10,16 +10,12 @@ public class PressurePlateTriggerScene : MonoBehaviour
 
     private bool isActive = false; // whether the trigger is currently active
 
-    public AudioClip buttonPush;
-    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             isActive = true;
         }
-
-        AudioSource.PlayClipAtPoint(buttonPush, transform.position);
     }
 
     private void OnTriggerExit(Collider other)
